84d82d95316374f1716b19eae8379d5074e40713
Zak Cronin <71699711+NyanZak@users.noreply.github.com>
2022-05-17 02:08:23 +0100

    Add files via upload

---
+++ Assets/RadarContact.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarContact : MonoBehaviour
+{  
+    void Awake()
+    {
+        gameObject.layer = 6;
+    }
+}
